8127cf222c7ce581288af15d1da3132696b03297
Quja <Quja@DESKTOP-SES7F5N>
2021-05-18 16:14:46 +0500

    dsd

---
+++ src/main/java/laba4/app.js
@@ -0,0 +1,12 @@
+VANTA.WAVES({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  waveSpeed: 1.50,
+  zoom: 1.04
+})
